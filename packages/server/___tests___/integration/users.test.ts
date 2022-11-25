c12458c3684fbe4ed0814474860fdbc34a02d375
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2022-11-25 14:55:03 -0300

    feat: user tests to create new user.

--- packages/server/___tests___/integration/users.test.ts
+++ packages/server/___tests___/integration/users.test.ts
@@ -1,5 +1,7 @@
 const frisby = require('frisby');
 const path = require('path');
+const shell = require('shelljs');
+
 require('dotenv').config({ path: path.join(__dirname, '../../../../.env') });
 
 const data = require('../data/data.js');
@@ -12,14 +14,19 @@ const url =
     ? `http://${HOST}:${PORT}`
     : process.env.DEPLOY_URL;
 
+describe('Database update.', () => {
+  shell.exec('yarn db:drop');
+  shell.exec('yarn db:create && yarn db:migrate');
+});
+
 describe('Users tests.', () => {
-  it('It should be possible to list all users.', async () => {
+  it('When creating the database, it should not contain any data.', async () => {
     const result = await frisby.get(`${url}/users`).expect('status', 200);
 
-    expect(JSON.parse(result._body)).toEqual([]);
+    expect(JSON.parse(result._body)).toHaveLength(0);
   });
 
-  it.only('It must be possible to create a new user.', async () => {
+  it('It must be possible to create a new user.', async () => {
     const result = await frisby
       .post(`${url}/users`, data[0])
       .expect('status', 201);
@@ -27,5 +34,13 @@ describe('Users tests.', () => {
     expect(JSON.parse(result._body)).toEqual({
       message: 'User created successfully.',
     });
+  });
+
+  it('When inserting 1 user, it must contain 1 referring record.', async () => {
+    const result = await frisby.get(`${url}/users`).expect('status', 200);
+    console.log(result._body);
+
+    expect(JSON.parse(result._body)).toHaveLength(1);
+    expect(JSON.parse(result._body)[0].username).toEqual(data[0].username);
   });
 });
