67a30e5f4508f90c317b5d6b3b984f84bf0ec80e
Adriano Monteiro <adrianomonteiroweb@gmail.com>
2022-11-26 10:30:35 -0300

    feat: implement utils functions

---
+++ packages/server/utils/functions/httpStatusFunction.ts
@@ -0,0 +1,11 @@
+import { StatusCodes } from 'http-status-codes';
+
+export const {
+  OK,
+  BAD_REQUEST,
+  INTERNAL_SERVER_ERROR,
+  CREATED,
+  UNAUTHORIZED,
+  CONFLICT,
+  NOT_FOUND,
+} = StatusCodes;
